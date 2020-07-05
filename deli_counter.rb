cb95a0387382652c892ffeb90dd8de8ccaa81a7d
Ynda Jas <yndajas@gmail.com>
2020-07-05 16:50:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+
+require 'pry'
+
+def line(line)
+  if line == []
+    puts "The line is currently empty."
+  else
+    message = "The line is currently:"
+    line.each_with_index do |name,index|
+      message << " #{index+1}. #{name}"
+    end
+    binding.pry
+    puts message
+  end
+end
